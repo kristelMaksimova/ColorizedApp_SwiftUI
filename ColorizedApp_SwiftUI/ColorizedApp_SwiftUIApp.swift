3ee26ec7808424e957f182a41bb7b55e2babe87c
Maksimova Kristel <idmaksimovak@gmail.com>
2022-01-25 13:54:43 +0300

    Initial Commit

---
+++ ColorizedApp_SwiftUI/ColorizedApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SwiftUIApp.swift
+//  ColorizedApp_SwiftUI
+//
+//  Created by Kristi on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
